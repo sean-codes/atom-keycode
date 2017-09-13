6937a9972c064a56fdfc537708bed1603e84e71b
sean-codes <sean_codes@outlook.com>
2017-09-13 00:42:34 -0500

    init

---
+++ spec/atom-keycode-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomKeycodeView from '../lib/atom-keycode-view';
+
+describe('AtomKeycodeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
