64efdfa4ca0b42ece800df9f5cc588e6ccf35998
sean-codes <sean_codes@outlook.com>
2017-09-13 00:54:32 -0500

    remove spec and menus

--- spec/atom-keycode-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomKeycodeView from '../lib/atom-keycode-view';
-
-describe('AtomKeycodeView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
